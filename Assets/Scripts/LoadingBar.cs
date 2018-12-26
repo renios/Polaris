bca7f451b14b05cb4d7c0d384e5d5b364ba330bf
thiEFcat <fsda109238@gmail.com>
2018-12-26 18:06:46 +0900

    관측 화면 UI 설정 및 UnityUIExtensions 패키지 적용

---
+++ Assets/Scripts/LoadingBar.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadingBar : MonoBehaviour
+{
+    public RectTransform FillBody;
+
+    public void SetValue(float value)
+    {
+        FillBody.localScale = new Vector3(value / 9f * 10f, 1, 1);
+    }
+}
---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public static SceneChanger Instance { get; private set; }
+
+    public Animator Motion;
+    public LoadingBar Bar;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public static void ChangeScene(string sceneName, string motionName = "SceneFadeIn", float motionTime = 0.5f)
+    {
+        Instance.StartCoroutine(Instance.ChangeSceneSub(sceneName, motionName, motionTime));
+    }
+
+    public IEnumerator ChangeSceneSub(string sceneName, string motionName, float motionTime)
+    {
+        Motion.Play(motionName);
+        yield return new WaitForSeconds(motionTime);
+
+        AsyncOperation loading = SceneManager.LoadSceneAsync(sceneName);
+        loading.allowSceneActivation = false;
+        while(!loading.isDone)
+        {
+            Bar.SetValue(loading.progress);
+            if (loading.progress >= 0.9f)
+                loading.allowSceneActivation = true;
+            yield return null;
+        }
+    }
+}
