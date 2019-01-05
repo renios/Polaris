b6a413121020091182f63b29bd1614a7e92ca641
thiEFcat <fsda109238@gmail.com>
2019-01-05 14:04:33 +0900

    각종 오류 수정 시도

--- Assets/Scripts/Enums.cs
+++ Assets/Scripts/Enums.cs
@@ -4,26 +4,12 @@ using UnityEngine;
 
 public enum CharacterName
 {
-<<<<<<< HEAD
-	public enum CharacterName
-	{
-		베가,
-		멜리크,
-		아케르,
-		안카,
-		안타레스,
-		에니프,
-		폴라리스,
-        투반
-	}
-}
-=======
 	베가,
 	멜리크,
 	아케르,
 	안카,
 	안타레스,
 	에니프,
-	폴라리스
+	폴라리스,
+    투반
 }
->>>>>>> 0ef431b1a081b176970af8eb59695b886d164314
---
+++ Assets/Scripts/MainScene/CharacterObject.cs
@@ -0,0 +1,25 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterObject : MonoBehaviour
+{
+    public bool IsAnimating;
+
+    private void Update()
+    {
+
+    }
+
+    public IEnumerator MoveStraight(float delta, float duration, bool passingStair)
+    {
+        IsAnimating = true;
+        if (passingStair)
+            transform.Translate(0, 0, 0.2f);
+        yield return transform.DOMove(transform.position + new Vector3(delta, 0, 0), duration);
+        if (passingStair)
+            transform.Translate(0, 0, -0.2f);
+        IsAnimating = false;
+    }
+}
