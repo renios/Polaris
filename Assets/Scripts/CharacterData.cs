7921572859b25632ad4de581817a4063680bbd37
thiEFcat <fsda109238@gmail.com>
2019-01-16 19:15:50 +0900

    캐릭터 / 카드 구조체

---
+++ Assets/Scripts/CharacterData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// '캐릭터'의 데이터입니다. (개별 카드 데이터와는 다릅니다)
+/// </summary>
+public struct CharacterData
+{
+    public int CharNumber;
+    public string Name;
+    public string InternalName;
+    public float Lux;
+    public float LYDistance;
+    public string Description;
+}
