844403cb0cb62630879cc858c523b4d8cf2b7b48
Bae yoo bin <yb_zzang@snu.ac.kr>
2019-02-02 18:57:02 +0900

    좌측 상단 재화 표시 UI

---
+++ Assets/Scripts/MainScene/GemManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GemManager : MonoBehaviour {
+
+    public static GemManager Instance { get; set; }
+    private static int Gem1;
+    private static int Gem2;
+    public Text GemText1;
+    public Text GemText2;
+
+    private void Awake()
+    {
+        Instance = this;
+        //temp
+        Gem1 = 1000; Gem2 = 1000;
+        ShowState();
+    }
+
+    public void SaveState(bool gem, int num)
+    {
+        if (gem)
+        {
+            Gem1 += num;
+            if (Gem1 > 99999) Gem1 = 99999;
+            else if (Gem1 < 0) Gem1 = 0;
+            ShowState();
+        }
+        else
+        {
+            Gem2 += num;
+            if (Gem2 > 99999) Gem2 = 99999;
+            else if (Gem2 < 0) Gem2 = 0;
+            ShowState();
+        }
+    }
+
+    private void ShowState()
+    {
+        GemText1.text = Gem1.ToString();
+        GemText2.text = Gem2.ToString();
+    }
+
+}
