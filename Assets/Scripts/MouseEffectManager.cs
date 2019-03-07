76b78b9c00291bb3c1c5c0d046a7fe3689af11fe
Minjung Kim <reny.atkins@gmail.com>
2019-03-08 01:50:40 +0900

    마우스 클릭 이펙트 추가

---
+++ Assets/Scripts/MouseEffectManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseEffectManager : MonoBehaviour {
+
+	public GameObject Effect;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			Instantiate(Effect, new Vector3(mousePosition.x, mousePosition.y, 0), Quaternion.identity);
+		}
+	}
+}
