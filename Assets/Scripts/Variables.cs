eb24f56fd7b7e54fa2fd1354fbcef57890bf0758
Neal Shaw <nepsis.96@gmail.com>
2019-03-16 13:55:53 +0900

    가챠씬 개발중
    - 가챠 flow 구현 완료
    - Null image 임시로 추가 (Triangle 비스무리한 자리를 복사해서 재탕)

--- Assets/Scripts/Variables.cs
+++ Assets/Scripts/Variables.cs
@@ -11,6 +11,8 @@ public static class Variables
 
     public static bool CameraMove;
 
+    public static int btnState = 0; // 0: 관측시작, 1: 관측중, 2: 관측완료
+
     #region Variables for dialogue scene
     public static int DialogCharIndex;          // 대화 씬에서 참조할 캐릭터 번호
     public static int DialogCardIndex;          // 대화 씬에서 참조할 캐릭터 내 종류 번호
